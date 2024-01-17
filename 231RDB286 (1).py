b52a08d7df98316e55ba465e03f3bd8edc11cc26
231RDB286 <144678922+RaitisRegza@users.noreply.github.com>
2024-01-17 13:35:27 +0000

    exception

--- 231RDB286 (1).py
+++ 231RDB286 (1).py
@@ -16,36 +16,41 @@ if specific_country == 'yes':
 
         driver.get("https://www.yr.no/en")
         WebDriverWait(driver, 10)
-
-        search = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "page-header__search-button")))
-        search.click()
-
-        time.sleep(1)
-
-        search_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "page-header__search-input")))
-        WebDriverWait(driver, 1)
-        search_box.send_keys(city)
-
-        time.sleep(1)
-
-        first_result = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "search__suggested-item")))
-        first_result.click()
         
-        find = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "daily-weather-list-item__forecast")))
-        temp = find.text
-        driver.quit()
+        try:
 
-        temp = temp.split('\n')
-        pos1 = [line.split() for line in temp]
+                search = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "page-header__search-button")))
+                search.click()
 
-        # Create a pandas DataFrame from the data
-            
-            
-        df = pd.DataFrame(list(zip(pos1)), columns = ['[temperature:]']) 
-        removed_df = df.drop(df.index[0])
-        print(removed_df)
+                time.sleep(1)
 
-        driver.quit()
+                search_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "page-header__search-input")))
+                WebDriverWait(driver, 1)
+                search_box.send_keys(city)
+
+                time.sleep(1)
+
+                first_result = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "search__suggested-item")))
+                first_result.click()
+                
+                find = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "daily-weather-list-item__forecast")))
+                temp = find.text
+                driver.quit()
+
+                temp = temp.split('\n')
+                pos1 = [line.split() for line in temp]
+
+                # Create a pandas DataFrame from the data
+                
+                
+                df = pd.DataFrame(list(zip(pos1)), columns = ['[temperature:]']) 
+                removed_df = df.drop(df.index[0])
+                print(removed_df)
+
+                driver.quit()
+        except TimeoutException:
+                print("Country not found or does not exist.")
+                driver.quit()
            
-elif specific_country == 'no' or 'NO':
+elif specific_country == 'no':
         print('TAVA MAMMA')
